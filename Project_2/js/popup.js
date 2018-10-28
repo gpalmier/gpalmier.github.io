968de43f12cb81e3bb20a601f0fcd7d7c1179f7b
gpalmier <gpalmier@nd.edu>
2018-10-28 18:19:27 -0400

    Project 2 Posting
    Posting content for project 2

---
+++ Project_2/js/popup.js
@@ -0,0 +1,183 @@
+//////////////////////NORMAL MODALS////////////////////
+// Button to open a modal
+$(document).ready(function (){
+    $('#button1_normal').click(function() {
+        $("#modal-content_step1_normal").toggleClass("visible")
+        });
+});
+
+// Button to close a modal
+$(document).ready(function (){
+    $('#modal-content_step1_normal_close').click(function() {
+        $("#modal-content_step1_normal").toggleClass("visible")
+        });
+});
+
+
+// Button to open a modal
+$(document).ready(function (){
+    $('#button2_normal').click(function() {
+        $("#modal-content_step2_normal").toggleClass("visible")
+        });
+});
+
+// Button to close a modal
+$(document).ready(function (){
+    $('#modal-content_step2_normal_close').click(function() {
+        $("#modal-content_step2_normal").toggleClass("visible")
+        });
+});
+
+
+// Button to open a modal
+$(document).ready(function (){
+    $('#button3_normal').click(function() {
+        $("#modal-content_step3_normal").toggleClass("visible")
+        });
+});
+
+// Button to close a modal
+$(document).ready(function (){
+    $('#modal-content_step3_normal_close').click(function() {
+        $("#modal-content_step3_normal").toggleClass("visible")
+        });
+});
+
+
+
+//////////////////////DRY MODALS////////////////////
+// Button to open a modal
+$(document).ready(function (){
+    $('#button1_dry').click(function() {
+        $("#modal-content_step1_dry").toggleClass("visible")
+        });
+});
+
+// Button to close a modal
+$(document).ready(function (){
+    $('#modal-content_step1_dry_close').click(function() {
+        $("#modal-content_step1_dry").toggleClass("visible")
+        });
+});
+
+
+// Button to open a modal
+$(document).ready(function (){
+    $('#button2_dry').click(function() {
+        $("#modal-content_step2_dry").toggleClass("visible")
+        });
+});
+
+// Button to close a modal
+$(document).ready(function (){
+    $('#modal-content_step2_dry_close').click(function() {
+        $("#modal-content_step2_dry").toggleClass("visible")
+        });
+});
+
+
+// Button to open a modal
+$(document).ready(function (){
+    $('#button3_dry').click(function() {
+        $("#modal-content_step3_dry").toggleClass("visible")
+        });
+});
+
+// Button to close a modal
+$(document).ready(function (){
+    $('#modal-content_step3_dry_close').click(function() {
+        $("#modal-content_step3_dry").toggleClass("visible")
+        });
+});
+
+//////////////////////OILY MODALS////////////////////
+// Button to open a modal
+$(document).ready(function (){
+    $('#button1_oily').click(function() {
+        $("#modal-content_step1_oily").toggleClass("visible")
+        });
+});
+
+// Button to close a modal
+$(document).ready(function (){
+    $('#modal-content_step1_oily_close').click(function() {
+        $("#modal-content_step1_oily").toggleClass("visible")
+        });
+});
+
+
+// Button to open a modal
+$(document).ready(function (){
+    $('#button2_oily').click(function() {
+        $("#modal-content_step2_oily").toggleClass("visible")
+        });
+});
+
+// Button to close a modal
+$(document).ready(function (){
+    $('#modal-content_step2_oily_close').click(function() {
+        $("#modal-content_step2_oily").toggleClass("visible")
+        });
+});
+
+
+// Button to open a modal
+$(document).ready(function (){
+    $('#button3_oily').click(function() {
+        $("#modal-content_step3_oily").toggleClass("visible")
+        });
+});
+
+// Button to close a modal
+$(document).ready(function (){
+    $('#modal-content_step3_oily_close').click(function() {
+        $("#modal-content_step3_oily").toggleClass("visible")
+        });
+});
+
+//////////////////////COMBO MODALS////////////////////
+// Button to open a modal
+$(document).ready(function (){
+    $('#button1_combo').click(function() {
+        $("#modal-content_step1_combo").toggleClass("visible")
+        });
+});
+
+// Button to close a modal
+$(document).ready(function (){
+    $('#modal-content_step1_combo_close').click(function() {
+        $("#modal-content_step1_combo").toggleClass("visible")
+        });
+});
+
+
+// Button to open a modal
+$(document).ready(function (){
+    $('#button2_combo').click(function() {
+        $("#modal-content_step2_combo").toggleClass("visible")
+        });
+});
+
+// Button to close a modal
+$(document).ready(function (){
+    $('#modal-content_step2_combo_close').click(function() {
+        $("#modal-content_step2_combo").toggleClass("visible")
+        });
+});
+
+
+// Button to open a modal
+$(document).ready(function (){
+    $('#button3_combo').click(function() {
+        $("#modal-content_step3_combo").toggleClass("visible")
+        });
+});
+
+// Button to close a modal
+$(document).ready(function (){
+    $('#modal-content_step3_combo_close').click(function() {
+        $("#modal-content_step3_combo").toggleClass("visible")
+        });
+});
+
+
